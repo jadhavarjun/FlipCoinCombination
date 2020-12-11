912a56f4ac646a860e60c680b53a0f9c8dd6ffa7
arjunjava <jadhavarjun53@gmail.com>
2020-12-11 12:38:34 +0530

    Add : code to simulator loop through multiple times of flipping a coin to store the single combination in a Dictionary and print percentage

--- flipCoinComb.sh
+++ flipCoinComb.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-echo "Welcome flipcoincombination"
+read -p "Enter how number for flipping the coin" number
 heads=1
 tails=0
+headCount=0
+tailCount=0
+count=$number
+declare A dictionary
+while (($count>0))
+do
+	flipCoin=$(( RANDOM%2 ))
 
-flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq 1 ]
+	then
+		dictionary[$count]="heads"
+		((headCount++))
+		#headPercent=$((($headCount*100)/$number))
+		#echo "Percentage" $headPercent
+	else
+		dictionary[$count]="tails"
+		((tailCount++))
+		#tailPercent=$((($tailCount*100)/$number))
+		#echo "Percentage" $tailPercent
+	fi
+((count--))
+done
+echo ${dictionary[@]}
+headPercent=$((($headCount*100)/$number))
+tailPercent=$((($tailCount*100)/$number))
+echo "Percentage of Head : " $headPercent
+echo "Percentage of Tail : " $tailPercent
 
-if [ $flipCoin -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+#headPercent=$(echo "scale=2; ($headCount*100)/$number"|bc)
+#tailPercent=$(echo "scale=2; ($tailCount*100)/$number"|bc)
